c2df955a259490e0afeadba77e112588ac6ea9e6
Kevin Komro <kkomro1993@gmail.com>
2025-01-27 09:37:24 -0600

    Complete Exericse 02 - repeatString
    Complete logic in repeatString function.
    Remove .skip(s) in spec.ts file.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(text, iterator) {
+    let newString = '';
 
+    if(iterator < 0) {
+        return 'ERROR';
+    };
+
+    for(let i = 1; i <= iterator; i++) {
+        newString += text;
+    }
+    return newString;
 };
 
 // Do not edit below this line
